c3d76749d2e44f3d41b392148c211494f7c95d67
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-23 20:35:35 +0000

    Set up environment for project

---
+++ index.js
@@ -0,0 +1,126 @@
+/* eslint-disable max-classes-per-file */
+import { Books } from "./modules/books.js";
+const form = document.querySelector('.form');
+const booksDiv = document.querySelector('.books');
+const addNew = document.querySelector('.add-book');
+const viewList = document.querySelector('.my-grid');
+const viewContact = document.querySelector('.contact-info');
+const listLink = document.querySelectorAll('.link-1');
+const AddLink = document.querySelectorAll('.link-2');
+const contactLink = document.querySelectorAll('.link-3');
+const dateContainer = document.querySelector('.date');
+const date = new Date().toLocaleDateString('en-us', { month: 'long', day: '2-digit', year: 'numeric' });
+const time = new Date().toLocaleTimeString('en-us', { hour: 'numeric', minute: '2-digit' });
+const dateTime = `${date} ${time}`;
+
+dateContainer.innerHTML = dateTime;
+
+listLink.forEach((link) => link.addEventListener('click', () => {
+  viewList.style.display = 'flex';
+  addNew.style.display = 'none';
+  viewContact.style.display = 'none';
+}));
+
+AddLink.forEach((link) => link.addEventListener('click', () => {
+  addNew.style.display = 'flex';
+  viewList.style.display = 'none';
+  viewContact.style.display = 'none';
+}));
+
+contactLink.forEach((link) => link.addEventListener('click', () => {
+  viewContact.style.display = 'flex';
+  viewList.style.display = 'none';
+  addNew.style.display = 'none';
+}));
+
+
+
+class CreateNewBook {
+  static addNewBook(book) {
+    const bookUnit = document.createElement('li');
+    bookUnit.className = 'books-li';
+    bookUnit.id = book.id;
+    bookUnit.innerHTML = `
+        <p class="book-name">${book.title}</p> 
+        <p>By</p>
+        <p class="the-auhtor">${book.author}</p>
+        <button class='removeBook'>Remove</button>
+        `;
+    booksDiv.appendChild(bookUnit);
+    booksDiv.style.border = '3px solid black';
+  }
+
+  static removeBookFromPage(target) {
+    if (target.classList.contains('removeBook')) {
+      target.parentElement.remove();
+    }
+
+    if (!booksDiv.firstElementChild) {
+      booksDiv.style.border = '3px solid white';
+    }
+  }
+
+  static loadFromStorage() {
+    let books;
+
+    if (localStorage.getItem('bookInfo')) {
+      books = JSON.parse(localStorage.getItem('bookInfo'));
+    } else {
+      books = [];
+    }
+
+    return books;
+  }
+
+  static displayBooksFromStorage() {
+    const books = CreateNewBook.loadFromStorage();
+
+    books.forEach((book) => {
+      CreateNewBook.addNewBook(book);
+    });
+  }
+
+  static removeBookFromStorage(element) {
+    const books = CreateNewBook.loadFromStorage();
+    const { id } = element.parentElement;
+    const index = books.findIndex((book) => book.id === id);
+    books.splice(index, 1);
+    localStorage.setItem('bookInfo', JSON.stringify(books));
+  }
+
+  static displayOnLoad() {
+    // if ()
+    viewList.style.display = 'flex';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const titleInput = document.querySelector('.book-title');
+  const authorInput = document.querySelector('.book-author');
+  const title = document.querySelector('.book-title').value;
+  const author = document.querySelector('.book-author').value;
+  const books = CreateNewBook.loadFromStorage();
+
+  if (!title || !author) {
+    addNew.style.display = 'flex';
+  } else {
+    const book = new Books(title, author, Date.now());
+    books.push(book);
+    CreateNewBook.addNewBook(book);
+    CreateNewBook.loadFromStorage();
+
+    localStorage.setItem('bookInfo', JSON.stringify(books));
+
+    titleInput.value = '';
+    authorInput.value = '';
+  }
+});
+
+booksDiv.addEventListener('click', (e) => {
+  CreateNewBook.removeBookFromPage(e.target);
+  CreateNewBook.removeBookFromStorage(e.target);
+});
+
+window.addEventListener('load', CreateNewBook.displayBooksFromStorage());
+window.addEventListener('load', CreateNewBook.displayOnLoad());
